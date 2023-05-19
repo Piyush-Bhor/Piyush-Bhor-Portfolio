7a2b74e2ce7de818e1060d21edbc75024a9a5b76
Piyush Bhor <131725874+Piyush-Bhor@users.noreply.github.com>
2023-05-19 15:00:19 -0400

    Edidted data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "EJS, MongoDB, Express, Node",
     description:
     "StrainQuest is a fun game, that suggets you new and random cannabis strains to discover and try. You can collect these strain, search for the ones you have already tried and add them to your collection. It's a pokedex for weed!",
-    image: "./project-1.gif",
+    image: "./StrainQuest.png",
     link: "https://github.com/Piyush-Bhor/StrainQuest",
   },
   {
@@ -12,41 +12,24 @@ export const projects = [
     subtitle: "EJS, MongoDB, Express, Node",
     description:
     "DishSavvy is a web application that allows users to easily search for recipes that align with their dietary needs. Users can view recipes and create an account to save recipes for later. This project was originally made for the Smart Cities 2023 hackathon.",
-    image: "./project-2.gif",
+    image: "./DishSavvy.png",
     link: "https://github.com/Piyush-Bhor/CC_SenecaHackathon",
   },
   {
     title: "Portfolio",
     subtitle: "React and Tailwind",
     description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
+    "This is my Portfolio made using React and Tailwind.css. It is hosted on Github pages. Feel free to checkout my GitHub repo as well to see more of my work.",
+    image: "./Portfolio.png",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "HighFinder",
+    subtitle: "Python Flask",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
-];
-
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+      "A REST API build using sklearn's ML module to get similiar cannabis strains. You can also search strains based on their name, effects, flavours, type, rating and description. The dataset was acquired from Kaggle.",
+    image: "./The Cannabis API.png",
+    link: "https://github.com/Piyush-Bhor/HighFinder",
   },
 ];
 
